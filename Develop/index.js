8390032edaa2b247e3776e80958f8cd1fd904e21
pguerra98 <119546662+pguerra98@users.noreply.github.com>
2023-03-03 18:51:48 -0500

    added in code to both index & generateMarkdown javascript files, overall I was able to get the base down and get the questions to prompt to the user when a user runs node index, however there are multiple bugs still present that needs to be fixed, and the readme file is currently empty, just an udpate push for now

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,161 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
 
-// TODO: Create a function to write README file
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+const fs = require ('fs');
+
+//an array of questions for user input
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What would you like to name the title of your project?',
+        validate: q1input => {
+            if (q1input) {
+                return true;
+            } else {
+                console.log('Please try entering your title again.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'GitUsername',
+        message: 'Please provide your GitHub username',
+        validate: q2input => {
+            if (q2input) {
+                return true;
+            } else {
+                console.log('Please try entering your GitHub username again.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'info',
+        message: 'Describe your project and what goal it is trying to achieve',
+        validate: q3input => {
+            if (q3input) {
+                return true;
+            } else {
+                console.log('Please provide information about your project.');
+                return false;
+            }
+        }
+    
+    },
+
+    {
+
+        type: 'input',
+        name: 'installation',
+        message: 'Describe steps on how to install your project',
+        validate: q4input => {
+            if (q4input) {
+                return true;
+            } else {
+                console.log('Please provide information about the installation regarding your project.');
+                return false;
+            }
+        }
+    
+    },
+ 
+    {
+
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions on how your project will be utilized',
+        validate: q5input => {
+            if (q5input) {
+                return true;
+            } else {
+                console.log('Please provide information about the steps on how to use your project.');
+                return false;
+            }
+        }
+    
+    },
+ 
+    
+    {
+
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license to use for your project, if no license is needed, select no license',
+        choices: ['MIT', 'Apache', 'no license', 'agpl']
+    },
+
+    {
+
+        type: 'input',
+        name: 'tests',
+        message: 'Provide instructions on how to test the project',
+        validate: q6input => {
+            if (q6input) {
+                return true;
+            } else {
+                console.log('Please provide information on how to test the project.');
+                return false;
+            }
+        }
+    
+    },
+];
+
+//  function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./infowriting/created-readme.md', fileContent, err => {
+            
+            if (err) {
+                reject(err);
+                return;
+            }
+            
+            resolve ({
+                ok: true,
+                message: 'File has been created!'
+            });
+        }
+        );
+    });
+};
+
+// function to initialize app
+function init() {
+
+    return inquirer.prompt(questions)
+    .then (readmeData => {
+        return readmeData;
+    })
+}
 
 // Function call to initialize app
-init();
+init()
+
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+
+.then (pagemd => {
+    return writeFile(pagemd);
+})
+
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+
+.catch(err => {
+    console.log(err);
+})
---
+++ Develop/infowriting/created-readme.md
@@ -0,0 +1,34 @@
+# README Generator
+
+  #  
+
+  ## Table-of-Contents
+
+  [Descrition](#description)
+  
+  [Installation](#installation)
+
+  [Usage](#usage)
+  
+ 
+
+[Tests](#tests)
+
+## [Description](#table-of-contents)
+
+This project is trying to help users generate a readme file
+
+## [Installation](#table-of-contents)
+
+First, if given an inquirer error when first attempting to run 'nodex index', type in ther terminal "npm i", and that should fix the issue.
+
+## [Usage](#table-of-contents)
+
+A user will come in and provide answers to a series of questions, some about their own personal information (GitHub username), and other questions pertaining to the project specifically.
+
+ 
+
+## [Tests](#table-of-contents)
+
+undefined
+
